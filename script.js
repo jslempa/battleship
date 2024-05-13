2658027497e38660206e38233a8b1011000046e8
Johanna Slempa <jslempa@gmail.com>
2024-05-13 11:25:59 -0400

    created player and cpu game boards

--- script.js
+++ script.js
@@ -26,8 +26,10 @@ const cpuBottomGridArray = []
 
 /*------------------------ Cached Element References ------------------------*/
 
-const topGridEl = document.querySelector('#top-grid')
-const bottomGridEl = document.querySelector('#bottom-grid')
+const playerTopGridEl = document.querySelector('#player-top-grid')
+const playerBottomGridEl = document.querySelector('#player-bottom-grid')
+const cpuTopGridEl = document.querySelector('#cpu-top-grid')
+const cpuBottomGridEl = document.querySelector('#cpu-bottom-grid')
 const carrierEl = document.querySelector('#carrier')
 const battleshipEl = document.querySelector('#battleship')
 const destroyerEl = document.querySelector('#destroyer')
@@ -48,126 +50,126 @@ const init = () => {
     renderBoard()
 }
 
-const renderBoard = () => {                                     //generates board with x and y axis 1 through 10
+
+
+//generates 4 boards with x and y axis 1 through 10          
+//create 4 boards- display all for testing- remove divs from cpu boards for final product
+const renderBoard = () => {                                    
     for (let i=1; i<=gridSize; i++) {                           
         for (let j=1; j<=gridSize; j++) {
-            playerTopGridArray.push(new topGridCell(i, j))
-            const div = document.createElement('div')           //create divs in constructor instead?
-            topGridEl.appendChild(div)
-            
-            
-        }
-    }
-    for (let i=1; i<=gridSize; i++) {                           
-        for (let j=1; j<=gridSize; j++) {
-            playerBottomGridArray.push(new bottomGridCell(i, j))
-            const div = document.createElement('div')
-            bottomGridEl.appendChild(div)
+            playerTopGridArray.push(new gridCell(i,j, playerTopGridEl))
+            playerBottomGridArray.push(new gridCell(i,j,playerBottomGridEl))
+            cpuTopGridArray.push(new gridCell(i,j,cpuTopGridEl))
+            cpuBottomGridArray.push(new gridCell(i,j,cpuBottomGridEl))
+            // const div = document.createElement('div')               //create divs in constructor instead?
+            // topGridEl.appendChild(div)
         }
     }
 }    
 
-const updateBoard = (?) => {}                                    // updates board after each turn
-                                                                
+//const updateBoard = (?) => {} // updates board after each turn                                                      
+
 
 
 
 // top grid cell object constructor
- function topGridCell(row, col) {
+ function gridCell(row, col, element) {
     this.row = row
     this.col = col
     this.selected = false
     this.occupied = false
     this.hit = false
     this.ship = null
-    document.createElement
-
-
-    this.addEventListener('click', handleClick)                 // this needs to be added to an html element (div)
+    const divEl = document.createElement('div')
+    divEl.dataset.row = row
+    divEl.dataset.col = col
+    element.appendChild(divEl)
+    //divEl.addEventListener('click', handleClick)                 
 }
 
-// bottom grid cell object constructor
-function bottomGridCell(row,col) {
-    this.row = row
-    this.col = col
-    this.selected = false
-    this.occupied = false
-    this.hit = false
-    this.ship = null
-    // what properties do the bottom grid cells need?
-}
+// // commented out bc I don't think I need a cell constructor for each board
+// function bottomGridCell(row,col) {
+//     this.row = row
+//     this.col = col
+//     this.selected = false
+//     this.occupied = false
+//     this.hit = false
+//     this.ship = null
+//     // what properties do the bottom grid cells need?
+// }
 
 
 
-const selectShip = () => {}
+// const selectShip = () => {}
 
-const updateMessage = (?) => {}
+// const updateMessage = (?) => {}
 
-const changeTurn = (turn) => {}
+// const changeTurn = (turn) => {}
  
 
-// what needs to change on the top board?
-// what need to change on the bottom board?
-const handleClick = (event) => {                                // player picks a square
-    if (event.target.selected === true) {                       // square is already picked
-        updateMessage(?) 'pick another square'                  // tell player to pick again
-    } else {                                                    // square is not already picked
-        event.target.selected = true                            // mark square as selected
-        if (event.target.occupied === false) {                  // square does not have a ship
-            updateMessage(?) 'miss'                             // tell player it was a miss
-            event.target.hit = false  
-            //change board to display miss                           // mark square as miss                           
-        } else {                                                // square has a ship
-            event.target.hit = true                             // mark square as hit
-            //change board to display miss
-            run hitShip
+// // what needs to change on the top board?
+// // what need to change on the bottom board?
+// // player is clicking on the html div NOT the js array cell
+// const handleClick = (event) => {                                // player picks a square
+//     if (event.target.selected === true) {                       // square is already picked
+//         updateMessage(?) 'pick another square'                  // tell player to pick again
+//     } else {                                                    // square is not already picked
+//         event.target.selected = true                            // mark square as selected
+//         if (event.target.occupied === false) {                  // square does not have a ship
+//             updateMessage(?) 'miss'                             // tell player it was a miss
+//             event.target.hit = false  
+//             //change board to display miss                           // mark square as miss                           
+//         } else {                                                // square has a ship
+//             event.target.hit = true                             // mark square as hit
+//             //change board to display miss
+//             run hitShip
            
             
             
-        } 
-        turn = ('player') ? turn = 'computer' : turn = 'player' // changes turn
+//         } 
+//         turn = ('player') ? turn = 'computer' : turn = 'player' // changes turn
                                            
-    }                                        
+//     }                                        
     
-}
+// }
 
-const hitShip = (ship) => {
-    ship.hitCounter++
-    if (ship.hitCounter === ship.health && turn === 'player') {
-        playerShipCount--
-        checkForWinner()    
-    } else {
-        return
-    }
+// const hitShip = (ship) => {
+//     ship.hitCounter++
+//     if (ship.hitCounter === ship.health && turn === 'player') {
+//         playerShipCount--
+//         checkForWinner()    
+//     } else {
+//         return
+//     }
 
-}
+// }
 
-const checkIfSunk = (?) => {} // might not need this if I have hitShip
+// const checkIfSunk = (?) => {} // might not need this if I have hitShip
 
-const checkForWinner = () => {
-    if (playerShipCount === 0) {
-        winner = computer
-        updateMessage()
-    } else if (cpuShipCount === 0) {
-        winner = player
-        updateMessage()
-    } else {
-        return
-    }
-
-
-}    
+// const checkForWinner = () => {
+//     if (playerShipCount === 0) {
+//         winner = computer
+//         updateMessage()
+//     } else if (cpuShipCount === 0) {
+//         winner = player
+//         updateMessage()
+//     } else {
+//         return
+//     }
 
 
+// }    
 
-/*----------------------------- Event Listeners -----------------------------*/
 
-// for clicking on each ship at the start of the game to select and place them
-carrierEl.addEventListener('click', selectShip)
-battleshipEl.addEventListener('click', selectShip)
-destroyerEl.addEventListener('click', selectShip)
-submarineEl.addEventListener('click', selectShip)
-patrolBoatEl.addEventListener('click', selectShip)
+
+// /*----------------------------- Event Listeners -----------------------------*/
+
+// // for clicking on each ship at the start of the game to select and place them
+// carrierEl.addEventListener('click', selectShip)
+// battleshipEl.addEventListener('click', selectShip)
+// destroyerEl.addEventListener('click', selectShip)
+// submarineEl.addEventListener('click', selectShip)
+// patrolBoatEl.addEventListener('click', selectShip)
 
 
 
@@ -177,9 +179,9 @@ patrolBoatEl.addEventListener('click', selectShip)
 /*--------------------------- This runs the game ----------------------------*/
 
 init()
-console.log(bottomGridArray[0])
+console.log(playerBottomGridArray[0])
 
-// bottomGridArray.forEach((cell) => {
+// playerTopGridArray.forEach((cell) => {
 //     console.log(`${cell.row}, ${cell.col}`)
 // })
 
@@ -187,10 +189,22 @@ console.log(bottomGridArray[0])
 
 /*
 
-player clicks on div                    //how do I match the div that got clicked on to the corresponding array value?
+player clicks on div 
+each div needs an event listener
+    - on click -> find matching cell
+        - check cell properties
+        - update cell properties
+        - change div color based on cell properties
+
+
+function to match div id to corresponding array value (row, col)
+
+                                                    
 - if already selected
     - return
 - if not selected
+    - change to selected
+    - 
 
 
 
@@ -243,6 +257,17 @@ If hit
 
 
 Repeat for computer's turn
+
+
+one board constructor
+boards are objects
+    player or cpu
+    top or bottom
+    grid array
+    
+one cell constructor
+
+
 
 
 TO DO
